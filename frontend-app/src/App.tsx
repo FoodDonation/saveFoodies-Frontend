5bb5f25008b0923288a9c0c955bb29d00b7f3510
lahirusudesh <164189x@uom.lk>
2020-10-09 19:57:44 +0530

    added sign in, sign up and add beneficiary components

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+import Home from "./components/home.component";
+import AddBeneficiary from "./components/beneficiary/add-beneficiary.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">Foodies</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <NavDropdown title="Beneficiary" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/add-beneficiary">Add</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="ml-auto">
+              <NavDropdown title="My Account" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/">Edit Profile</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">Sign Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/add-beneficiary" component={AddBeneficiary} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
